68072566902728c33e97955c42d43e5f4f98958f
pyeres <py1@uw.edu>
2016-10-19 16:43:28 -0400

    remove hard-coded prior

--- toy_model.py
+++ toy_model.py
@@ -25,23 +25,23 @@ def log_likelihood(X, theta, phi):
     bin_indices[bin_indices == len(n)] = len(n) - 1
     n_counts = n[bin_indices]
 
-    P_X_theta = n_counts / np.sum(n_counts)
-    return np.log(np.sum(P_X_theta))
+    P_X_given_theta = n_counts / np.sum(n_counts)
+    return np.sum(np.log(P_X_given_theta))
 
-def compute_posterior(thetas, phi, X):
+def compute_posterior(thetas, phi, X, priors):
     """
     Compute P(theta | phi, X)
     """
     posteriors = []
-    for theta in thetas:
-
+    
+    for i in range(len(thetas)):
         # Find log(\prod_{i=1}^n P(X_i | t, phi)
-        log_like = log_likelihood(X, theta, phi)
+        log_like = log_likelihood(X, thetas[i], phi)
 
         # Log-Prior:
         # Assume P(theta | phi) is a gaussian with mean 1.0
         # and std phi
-        prior = np.log(norm.pdf(theta, 1.0, phi))
+        prior = np.log(priors[i])
         posteriors.append(log_like + prior)
 
     posteriors = np.array(posteriors)
@@ -49,24 +49,29 @@ def compute_posterior(thetas, phi, X):
     return np.sign(normalize) * posteriors / normalize
 
 def optimize_phi(posteriors, thetas):
+    # entropy of posteriors
     best_entropy = np.sum(posteriors * np.exp(posteriors))
+
     phis = np.linspace(0.5, 1.5, 20)
     inf_gain = []
 
     for phi in phis:
-        posteriors = compute_posterior(thetas, phi, samples)
+        posteriors = compute_posterior(thetas, phi, samples, posteriors)
         curr_entropy = np.sum(posteriors * np.exp(posteriors))
         inf_gain.append(best_entropy - curr_entropy)
     return phis[np.argmax(inf_gain)]
 
+
 phi = 0.1
+# generate a set of plausible thetas
 thetas = np.linspace(0.5, 1.5, 100)
 
 for i in range(10):
-
+    # generate the initial data sample (x_1, x_2, ..., x_1000)
     samples = black_box(1000, 1.0, phi)
 
     # Compute posterior.
-    posteriors = compute_posterior(thetas, phi, samples)
-    phi = optimize_phi(posteriors, thetas)
+    priors = [norm.pdf(theta, 1.0, 1.0) for theta in thetas]
+    posterior = compute_posterior(thetas, phi, samples, priors)
+    phi = optimize_phi(posterior, thetas)
     print(phi)
