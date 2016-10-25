3bf5173400016f78c0301449c0e81da7b40d0132
MechCoder <manojkumarsivaraj334@gmail.com>
2016-10-25 12:37:06 -0400

    Return log posterior

--- toy_model.py
+++ toy_model.py
@@ -29,27 +29,35 @@ def log_likelihood(X, theta, phi):
     bin_indices[bin_indices == len(n)] = len(n) - 1
     n_counts = n[bin_indices]
     P_X_given_theta = n_counts / np.sum(n_counts)
+    plt.clf()
     return np.sum(safe_ln(P_X_given_theta))
 
-def compute_posterior(thetas, phi, X, prior):
+def compute_log_posterior(thetas, phi, X, prior):
     """
     Compute P(theta | phi, X)
     """
     log_posterior = []
+    log_likelihoods = []
+    log_prior = np.log(prior)
 
     for i, theta in enumerate(thetas):
         # Find log(\prod_{i=1}^n P(X_i | t, phi)
         log_like = log_likelihood(X, theta, phi)
-        print(log_like)
-        log_prior = np.log(prior[i])
-        log_posterior.append(log_like + log_prior)
-    posterior = np.exp(log_posterior)
-    print(log_posterior)
-    print(posterior)
-    print(thetas[np.argmax(posterior)])
 
-    normalize = np.sum(posterior)
-    return posterior / normalize
+        log_posterior.append(log_like + log_prior[i])
+        log_likelihoods.append(log_like)
+
+    # plt.plot(thetas, log_likelihoods)
+    # plt.show()
+    #
+    # plt.clf()
+    # plt.plot(thetas, log_posterior)
+    # plt.show()
+
+    # print(log_likelihoods)
+    # print(thetas[np.argmax(log_posterior)])
+
+    return log_posterior
 
 def optimize_phi(posterior, thetas):
     # entropy of posterior
@@ -66,7 +74,7 @@ def optimize_phi(posterior, thetas):
             print("optimize_phi " + str(i))
             theta_best = np.argmax(posterior)
             toy_data = black_box(1000, theta_best, phi, i)
-            posterior = compute_posterior(thetas, phi, toy_data, posterior)
+            posterior = compute_log_posterior(thetas, phi, toy_data, posterior)
             curr_entropy = np.sum(posterior * np.log(posterior))
             inf_gain.append(best_entropy - curr_entropy)
             best_phys.append(phis[np.argmax(inf_gain)])
@@ -92,10 +100,10 @@ def do_real_experiments():
     # and std 0.1
     prior = norm.pdf(thetas, 1.0, 0.1)
 
-    toy_posterior = compute_posterior(thetas, true_phi, real_data, prior)
+    toy_posterior = compute_log_posterior(thetas, true_phi, real_data, prior)
 
     print("toy_posterior generated")
-    # best_phi = optimize_phi(toy_posterior, thetas)
+    best_phi = optimize_phi(toy_posterior, thetas)
     return best_phi
 
 best_phi = do_real_experiments()
