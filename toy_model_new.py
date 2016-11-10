b7117eec2763371611ca7e9aa2165c8d73fb7513
MechCoder <manojkumarsivaraj334@gmail.com>
2016-11-10 17:18:38 -0500

    Minor fixes

--- toy_model_new.py
+++ toy_model_new.py
@@ -88,22 +88,27 @@ def optimize_phi(phis, return_inf=False):
         # 3. Average across theta_i's.
 
         # Assume uniform prior for now.
-        # Draw 100 thetas
+        # Draw 100 thetas uniformly from the given range.
         pick_thetas = thetas[rng.randint(0, len(thetas), len(thetas))]
 
-        # Heuristic fixed bins to compute the empirical pdf given theta and phi.
+        # Set bins to a reasonable range to compute the empirical pdf
+        # of X given theta and phi.
         bins = np.linspace(-5.0, 5.0, 100)
         bin_width = (bins[1] - bins[0]) / 2.0
         X_s = (bins + bin_width)[:-1]
 
         P_X_given_theta_phi = np.zeros((n_thetas, len(bins) - 1))
         for theta_ind, theta in enumerate(pick_thetas):
-            # compute empirical P(X | theta, phi) for each theta, request 1000 samples,
-            # store the samples for that theta in a row of the n_sample matrix
+            # compute empirical P(X | theta, phi) for these bins
             P_X_given_theta_phi[theta_ind] = empirical_pdf(theta, phi, bins)[0]
 
+        # Compute P(X | phi) by averaging out P(X, theta | phi) across the
+        # sampled values of theta.
         P_X_given_phi = np.mean(P_X_given_theta_phi, axis=0)
         P_X_given_phi /= np.sum(P_X_given_phi)
+
+        # Since the bins are now discretized, sample 10000 values from
+        # a multinomial with probabilities P_X_given_phi.
         data = np.repeat(X_s, rng.multinomial(10000, P_X_given_phi, size=1)[0])
         eig = expected_information_gain(thetas, data, phi)
         eigs.append(eig)
